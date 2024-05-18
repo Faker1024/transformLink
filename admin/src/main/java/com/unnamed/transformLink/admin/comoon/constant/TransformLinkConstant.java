5f91c947b716b5036e859f771de0b41c8e663b4c
jack <3361935899@qq.com>
2024-05-18 20:54:55 +0800

    feature: 定义一些常亮

---
+++ admin/src/main/java/com/unnamed/transformLink/admin/comoon/constant/TransformLinkConstant.java
@@ -0,0 +1,8 @@
+package com.unnamed.transformLink.admin.comoon.constant;
+
+public class TransformLinkConstant {
+    public static String USERNAME = "username";
+    public static String PASSWORD = "password";
+    public static String TOKEN = "token";
+    public static String INFO = "info";
+}
