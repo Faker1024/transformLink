68f0b079f1ce3044157529fe6197028ccf2e8d46
jack <3361935899@qq.com>
2024-05-26 14:43:27 +0800

    feature: 添加布隆過濾器到新增短連接，防止重復生成

---
+++ project/src/main/java/com/unnamed/transformLink/project/config/RBloomFilterConfiguration.java
@@ -0,0 +1,24 @@
+package com.unnamed.transformLink.project.config;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 布隆过滤器配置
+ */
+@Configuration
+public class RBloomFilterConfiguration {
+
+    /**
+     * 防止用户注册查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("userRegisterCachePenetrationBloomFilter");
+        //预估元素数量，运行的误判率；评估网站 https://krisives.github.io/bloom-calculator/
+        cachePenetrationBloomFilter.tryInit(100000000L, 0.001);
+        return cachePenetrationBloomFilter;
+    }
+}
--- project/src/main/java/com/unnamed/transformLink/project/service/impl/LinkServiceImpl.java
+++ project/src/main/java/com/unnamed/transformLink/project/service/impl/LinkServiceImpl.java
@@ -1,22 +1,33 @@
 package com.unnamed.transformLink.project.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.unnamed.transformLink.project.comoon.convention.exception.ServiceException;
 import com.unnamed.transformLink.project.dao.entity.LinkDO;
 import com.unnamed.transformLink.project.dao.mapper.LinkMapper;
 import com.unnamed.transformLink.project.dto.req.LinkCreateReqDTO;
 import com.unnamed.transformLink.project.dto.resp.LinkCreateRespDTO;
 import com.unnamed.transformLink.project.service.LinkService;
 import com.unnamed.transformLink.project.toolkit.HashUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RBloomFilter;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 /**
  * 连接接口实现层
  */
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class LinkServiceImpl extends ServiceImpl<LinkMapper, LinkDO> implements LinkService {
+
+    private final RBloomFilter<String> bloomFilter;
+
     @Override
     public LinkCreateRespDTO createLink(LinkCreateReqDTO requestParam) {
         String shortLinkSuffix = HashUtil.hashToBase62(requestParam.getOriginUrl());
+        String fullShortLink = requestParam.getDomain() + "/" + shortLinkSuffix;
         LinkDO linkDO = LinkDO.builder().originUrl(requestParam.getOriginUrl())
                 .createdType(requestParam.getCreatedType())
                 .describe(requestParam.getDescribe())
@@ -24,14 +35,28 @@ public class LinkServiceImpl extends ServiceImpl<LinkMapper, LinkDO> implements 
                 .gid(requestParam.getGid())
                 .validDateType(requestParam.getValidDateType())
                 .validDate(requestParam.getValidDate())
-                .fullShortUrl(requestParam.getDomain() +"/" + shortLinkSuffix)
+                .fullShortUrl(fullShortLink)
                 .shortUri(shortLinkSuffix)
                 .build();
-        baseMapper.insert(linkDO);
+        try {
+            baseMapper.insert(linkDO);
+        }catch (DuplicateKeyException es){
+            log.error("短連接{}重複入庫", fullShortLink);
+            throw new ServiceException("短連接生成重複");
+        }
+        bloomFilter.add(shortLinkSuffix);
         return LinkCreateRespDTO.builder()
                 .originUrl(linkDO.getOriginUrl())
                 .fullShortUrl(linkDO.getFullShortUrl())
                 .gid(linkDO.getGid())
                 .build();
+    }
+
+    public String generateLinkSuffix(LinkCreateReqDTO requestParam) {
+        String linkSuffix = null;
+        do {
+            linkSuffix = HashUtil.hashToBase62(requestParam.getOriginUrl());
+        }while (bloomFilter.contains(linkSuffix));
+        return linkSuffix;
     }
 }
